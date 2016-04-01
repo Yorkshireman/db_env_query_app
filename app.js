5b08e355135c8b4b921b99369b22e1590dc1cb0c
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2016-04-01 12:27:07 +0100

    app prints the uri of the mongo service instance to the browser viewport

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+var cfenv = require("cfenv");
+var appEnv = cfenv.getAppEnv();
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  var services = appEnv.getServices();
+  var dbUri = services.graphql_registration_db.credentials.uri
+  res.send(dbUri);
 });
 
 app.listen(process.env.PORT);
